0bc559f3a13c7c1db117a5e8a53cc828a0c55875
Boomchick <1227674944@qq.com>
2023-07-31 20:28:18 +0800

    renew config

--- config.go
+++ config.go
@@ -21,6 +21,17 @@ type DBConfig struct {
 	//  IOType
 	//  Only support FileIO at the moment
 	IOType logfile.IOType
+	// DiscardBufferSize a channel will be created to send the older entry size when a key updated or deleted.
+	// Entry size will be saved in the discard file, recording the invalid size in a log file, and be used when log file gc is running.
+	// This option represents the size of that channel.
+	// If you got errors like `send discard chan fail`, you can increase this option to avoid it.
+	DiscardBufferSize int
+
+	// LogFileGCRatio if discarded data in log file exceeds this ratio, it can be picked up for compaction(garbage collection)
+	// And if there are many files reached the ratio, we will pick the highest one by one.
+	// The recommended ratio is 0.5, half of the file can be compacted.
+	// Default value is 0.5.
+	LogFileGCRatio float64
 }
 
 func DefaultDBConfig(path string) DBConfig {
@@ -30,5 +41,7 @@ func DefaultDBConfig(path string) DBConfig {
 		MaxLogFileSize:       defaultMaxLogFileSize,
 		LogFileMergeInterval: defaultLogFileMergeInterval,
 		IOType:               defaultIOType,
+		DiscardBufferSize:    8 << 20,
+		LogFileGCRatio:       0.5,
 	}
 }
